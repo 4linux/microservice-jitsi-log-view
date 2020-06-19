12d16a32d39d49808331cca1b34ee07f758eaf9b
Bryan A. S <bryanasdev000@gmail.com>
2020-06-19 19:03:33 -0300

    main.go: Add more routes

--- main.go
+++ main.go
@@ -1,22 +1,19 @@
 package main
 
+
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
+	"github.com/gorilla/mux"
 )
 
-// Pensamentos
-// Ultimos registros = v1/logs?last=25 -> Retorna os ultimos 25 registros
-// Buscar por curso = v1/logs?courseid=2 -> Retorna as aulas do curso com ID 2
-// Buscar por turma = v1/logs?groupid=8 -> Retorna as aulas do curso com ID 2
-// Buscar por aluno = v1/logs?student=alfa@domain.tld -> Retorna as aulas do curso com ID 2
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "microservice-jitsi-log-view")
 }
+
 
 func healthcheck(w http.ResponseWriter, r *http.Request) {
 	name, err := os.Hostname()
@@ -26,12 +23,55 @@ func healthcheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Awake and alive from %s", name)
 }
 
+
+func last(w http.ResponseWriter, r *http.Request) {
+	queryParams :=  r.URL.Query()
+	fmt.Fprintf(w, "Last %s", queryParams["last"][0])
+	// Trazer os ultimos N registros
+}
+
+
+func searchCourse(w http.ResponseWriter, r *http.Request) {
+	queryParams :=  r.URL.Query()
+	fmt.Fprintf(w, "Course %s", queryParams["courseid"][0])
+	// Trazer todas as turmas do curso N
+}
+
+
+func searchClass(w http.ResponseWriter, r *http.Request) {
+	queryParams :=  r.URL.Query()
+	fmt.Fprintf(w, "Class %s", queryParams["classid"][0])
+	// Trazer todas as aulas da turma N
+}
+
+
+func searchStudentEmail(w http.ResponseWriter, r *http.Request) {
+	queryParams :=  r.URL.Query()
+	fmt.Fprintf(w, "Student %s", queryParams["studentEmail"][0])
+	// TODO regex for email
+	// Trazer todos os registros do aluno@domain.tld
+}
+
+
+func searchLesson(w http.ResponseWriter, r *http.Request) {
+	queryParams :=  r.URL.Query()
+	fmt.Fprintf(w, "Class %s", queryParams["classid"][0])
+	fmt.Fprintf(w, "Lesson")
+	// Trazer todos os registros de aulas da turma N
+}
+
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler).Methods(http.MethodGet)
-	r.HandleFunc("/healthcheck", healthcheck).Methods(http.MethodGet)
-	api := r.PathPrefix("/v1").Subrouter()
-	api.HandleFunc("/logs", handler).Methods("GET")
-	http.Handle("/", r)
+	router := mux.NewRouter()
+	router.HandleFunc("/", handler).Methods(http.MethodGet)
+	router.HandleFunc("/healthcheck", healthcheck).Methods(http.MethodGet)
+	api := router.PathPrefix("/v1").Subrouter()
+	api.HandleFunc("/logs", last).Methods("GET").Queries("last", "{last:[0-9]+}")
+	api.HandleFunc("/logs", searchCourse).Methods("GET").Queries("courseid", "{last:[0-9]+}")
+	api.HandleFunc("/logs", searchClass).Methods("GET").Queries("classid", "{last:[0-9]+}")
+	api.HandleFunc("/logs", searchStudentEmail).Methods("GET").Queries("studentEmail", "{studentEmail}")
+	api.HandleFunc("/logs", searchLesson).Methods("GET").Queries("classid", "{last:[0-9]+}")
+	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	// TODO Log requests
 }
