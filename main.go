2d80930f00b12766c7a5fd98b8541aba81e9ef84
Bryan A. S <bryanasdev000@gmail.com>
2020-07-20 12:58:04 -0300

    main.go: Better logging + router

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -21,8 +22,11 @@ var URI_MONGODB string
 // Database to use for storing logs.
 var DATABASE string
 
-// Collection to user for storing logs.
+// Collection to use for storing logs.
 var COLLECTION string
+
+// Port to listen.
+var PORT string
 
 // Data structure as defined in https://github.com/bryanasdev000/microservice-jitsi-log .
 type Jitsilog struct {
@@ -38,14 +42,15 @@ type Jitsilog struct {
 
 // Setup of logs and database related configs.
 func init() {
+	log.Debug("microservice-jitsi-log-view init")
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
-	if len(os.Getenv("DEBUG")) > 0 {
+	log.SetReportCaller(true)
+	if len(os.Getenv("DEBUG")) > 0 && os.Getenv("DEBUG") == "true" {
 		log.SetLevel(log.DebugLevel)
 	} else {
 		log.SetLevel(log.InfoLevel)
 	}
-	log.SetReportCaller(true)
 	if len(os.Getenv("URI_MONGODB")) > 0 {
 		URI_MONGODB = os.Getenv("URI_MONGODB")
 	} else {
@@ -61,11 +66,17 @@ func init() {
 	} else {
 		COLLECTION = "logs"
 	}
-	log.Debug("microservice-jitsi-log-view init")
+	if len(os.Getenv("PORT")) > 0 && strings.HasPrefix(PORT, ":") == true {
+		PORT = os.Getenv("PORT")
+	} else {
+		PORT = ":8080"
+		log.Info("Port variable is missing or in wrong format (missing a colon ( : ) at start. It should be like ':8080'), using default one")
+	}
 	log.WithFields(log.Fields{
 		"URI":        URI_MONGODB,
-		"DATABASE":   DATABASE,
-		"COLLECTION": COLLECTION}).Info("Database Connection Info")
+		"Database":   DATABASE,
+		"Collection": COLLECTION}).Info("Database Connection Info")
+	log.Info("Listening at ", PORT)
 }
 
 // Creates and return a MongoDB client.
@@ -73,50 +84,65 @@ func getClient() *mongo.Client {
 	context, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(context, options.Client().ApplyURI(URI_MONGODB))
 	if err != nil {
-		log.Fatal("Failed to create the client ", err)
+		log.WithFields(log.Fields{
+			"error": err}).Fatal("Failed to create the Mongo client!")
 	}
 	return client
 }
 
 // Find logs with filter and ordered by decrescent timestamp, can limit & skip items in dataset.
-func findLogsFilter(size string, filter bson.D, skip string) []*Jitsilog {
+func findLogsFilter(size string, filter bson.D, skip string) (error, []*Jitsilog) {
 	client := getClient()
 	optFind := options.Find()
+	var jitsilogs []*Jitsilog
+	var logerror error
 	sizeInt, err := strconv.ParseInt(size, 10, 64)
 	if err != nil {
-		log.Fatal("Failed to convert size argument to int ", err)
+		log.WithFields(log.Fields{
+			"error": err}).Info("Failed to convert size argument to int")
+		logerror = err
+		return logerror, nil
 	}
 	skipInt, err := strconv.ParseInt(skip, 10, 64)
 	if err != nil {
-		log.Fatal("Failed to convert skip argument to int ", err)
+		log.WithFields(log.Fields{
+			"error": err}).Info("Failed to convert skip argument to int")
+		logerror = err
+		return logerror, nil
 	}
-	optFind.SetLimit(sizeInt)
 	log.Debug("Dataset row limit ", sizeInt)
 	log.Debug("Dataset row skip ", skipInt)
 	optFind.SetSkip(skipInt)
 	optFind.SetLimit(sizeInt)
 	optFind.SetSort(bson.D{{"timestamp", -1}})
-	var jitsilogs []*Jitsilog
+	// Count before search then do the math with size and skip args to counter index out of range
 	collection := client.Database(DATABASE).Collection(COLLECTION)
 	cursor, err := collection.Find(context.TODO(), filter, optFind)
 	if err != nil {
-		log.Fatal("Error on finding the documents ", err)
+		log.WithFields(log.Fields{
+			"error": err}).Info("Error on finding the documents")
+		logerror = err
+		return logerror, nil
 	}
 	log.Debug("Connection to MongoDB opened.")
 	for cursor.Next(context.TODO()) {
 		var jitsilog Jitsilog
 		err = cursor.Decode(&jitsilog)
 		if err != nil {
-			log.Fatal("Error on decoding the document ", err)
+			log.WithFields(log.Fields{
+				"error": err}).Info("Error on decoding the document")
+			logerror = err
+			return logerror, nil
 		}
 		jitsilogs = append(jitsilogs, &jitsilog)
 	}
 	err = client.Disconnect(context.TODO())
 	if err != nil {
-		log.Fatal("Failed to disconnect from database! ", err)
+		log.WithFields(log.Fields{
+			"error": err}).Fatal("Failed to disconnect from database!")
 	}
 	log.Debug("Connection to MongoDB closed.")
-	return jitsilogs
+	return logerror, jitsilogs
 }
 
 // Default handler, return the name of this service.
@@ -128,7 +154,8 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 func checkHealth(w http.ResponseWriter, r *http.Request) {
 	name, err := os.Hostname()
 	if err != nil {
-		log.Panic(err)
+		log.WithFields(log.Fields{
+			"error": err}).Fatal("Failed to get hostname!")
 	}
 	fmt.Fprintf(w, "Awake and alive from %s", name)
 }
@@ -136,8 +163,10 @@ func checkHealth(w http.ResponseWriter, r *http.Request) {
 // Query the latest logs with a variable dataset size based on the URL.
 func latestLogsHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
-	var jitsilogs []*Jitsilog
-	jitsilogs = findLogsFilter(queryParams["size"][0], bson.D{}, queryParams["skip"][0])
+	err, jitsilogs := findLogsFilter(queryParams["size"][0], bson.D{}, queryParams["skip"][0])
+	if err != nil {
+		log.Info(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(jitsilogs)
 }
@@ -145,9 +174,11 @@ func latestLogsHandler(w http.ResponseWriter, r *http.Request) {
 // Query all logs that correspond with desired courseid.
 func searchCourseHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
-	filter := bson.D{{"curso", queryParams["courseid"][0]}}
-	var jitsilogs []*Jitsilog
-	jitsilogs = findLogsFilter(queryParams["size"][0], filter, queryParams["skip"][0])
+	filter := bson.D{{"curso", queryParams["id"][0]}}
+	err, jitsilogs := findLogsFilter(queryParams["size"][0], filter, queryParams["skip"][0])
+	if err != nil {
+		log.Info(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(jitsilogs)
 }
@@ -155,9 +186,11 @@ func searchCourseHandler(w http.ResponseWriter, r *http.Request) {
 // Query all logs that correspond with desired classid
 func searchClassHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
-	filter := bson.D{{"turma", queryParams["classid"][0]}}
-	var jitsilogs []*Jitsilog
-	jitsilogs = findLogsFilter(queryParams["size"][0], filter, queryParams["skip"][0])
+	filter := bson.D{{"turma", queryParams["id"][0]}}
+	err, jitsilogs := findLogsFilter(queryParams["size"][0], filter, queryParams["skip"][0])
+	if err != nil {
+		log.Info(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(jitsilogs)
 }
@@ -165,9 +198,11 @@ func searchClassHandler(w http.ResponseWriter, r *http.Request) {
 // Query all logs that correspond with desired roomid
 func searchRoomHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
-	filter := bson.D{{"sala", queryParams["roomid"][0]}}
-	var jitsilogs []*Jitsilog
-	jitsilogs = findLogsFilter(queryParams["size"][0], filter, queryParams["skip"][0])
+	filter := bson.D{{"sala", queryParams["id"][0]}}
+	err, jitsilogs := findLogsFilter(queryParams["size"][0], filter, queryParams["skip"][0])
+	if err != nil {
+		log.Info(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(jitsilogs)
 }
@@ -175,9 +210,11 @@ func searchRoomHandler(w http.ResponseWriter, r *http.Request) {
 // Query all logs that correspond with desired student email
 func searchStudentHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
-	filter := bson.D{{"email", queryParams["studentEmail"][0]}}
-	var jitsilogs []*Jitsilog
-	jitsilogs = findLogsFilter(queryParams["size"][0], filter, queryParams["skip"][0])
+	filter := bson.D{{"email", queryParams["email"][0]}}
+	err, jitsilogs := findLogsFilter(queryParams["size"][0], filter, queryParams["skip"][0])
+	if err != nil {
+		log.Info(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(jitsilogs)
 }
@@ -186,12 +223,13 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", defaultHandler).Methods(http.MethodGet)
 	router.HandleFunc("/healthcheck", checkHealth).Methods(http.MethodGet)
-	api := router.PathPrefix("/v1").Subrouter()
-	api.HandleFunc("/logs", latestLogsHandler).Methods("GET").Queries("last", "{last:[0-9]+}")
-	api.HandleFunc("/logs", searchCourseHandler).Methods("GET").Queries("courseid", "{courseid}")
-	api.HandleFunc("/logs", searchClassHandler).Methods("GET").Queries("classid", "{classid}")
-	api.HandleFunc("/logs", searchStudentHandler).Methods("GET").Queries("studentEmail", "{studentEmail}")
-	api.HandleFunc("/logs", searchRoomHandler).Methods("GET").Queries("roomid", "{roomid}")
+	version := router.PathPrefix("/v1").Subrouter()
+	api := version.PathPrefix("/logs").Subrouter()
+	api.HandleFunc("/last", latestLogsHandler).Methods("GET")
+	api.HandleFunc("/course", searchCourseHandler).Methods("GET").Queries("id", "{id}")
+	api.HandleFunc("/class", searchClassHandler).Methods("GET").Queries("id", "{id}")
+	api.HandleFunc("/student", searchStudentHandler).Methods("GET").Queries("email", "{email}")
+	api.HandleFunc("/room", searchRoomHandler).Methods("GET").Queries("id", "{id}")
 	http.Handle("/", router)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
